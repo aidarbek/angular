23070930584c8e32650e95031980342e7a9fb2b5
aidarbek <aidarbek98@gmail.com>
2014-12-17 00:59:22 +0600

    new roles are adding to the list of all roles, code formatted better

--- app.js
+++ app.js
@@ -4,8 +4,9 @@
 */
 
 var App = angular.module('App', ['ui.select']);
-function AddUser($scope)
+function AddUser($scope, roles)
 {
+	//console.log(roles);
 	var url = ""; // we send POST request to
 					  // register new users to this URL
 					  // response should be in JSON
@@ -18,10 +19,12 @@ function AddUser($scope)
 			// If fields are not empty	
 			if($scope.userCreation.password == $scope.userCreation.passwordConfirm)
 			{
+				//$scope.userCreation.role = [];
+				//console.log($scope.userCreation);
 				//If password is confirmed
 				$scope.err = false;//There is no error
-				tmp = angular.copy($scope.userCreation);
-				tmp.editing = false;
+				//tmp = angular.copy($scope.userCreation);
+				//tmp.editing = false;
 				//Next line should be commented, till the BE is available 
 				/*
 					$http.post(url, tmp).success(function(data){
@@ -32,14 +35,22 @@ function AddUser($scope)
 						err = status + " error";
 					});
 				*/
-				//Delete next 4 lines, when BE available
-				tmp.user_id = 1;
-				$scope.Users.push(tmp);
-				$scope.userCreation = {role: []};
+				//Delete next 3 lines, when BE available
+				//tmp.user_id = 1;
+				//console.log("HUI!");
+				//console.log($scope.userCreation);
+				console.log($scope.userCreation.role);
+				console.log($scope.userCreation);
+				console.log(roles);
+				$scope.Users.push($scope.userCreation);
+				//console.log($scope.Users[$scope.Users.length - 1]);
+				$scope.Users[$scope.Users.length - 1].role = angular.copy($scope.userCreation.role);
+				//$scope.userCreation = {role: []};
+				//console.log($scope.Users);
 			}
 			else
 			{
-				$scope.err = "Password's are not equal!";
+				$scope.err = "Passwords are not equal!";
 			}
 		}
 		else
@@ -48,64 +59,30 @@ function AddUser($scope)
 		}
 }
 App.factory('Users', function () {
-  return [{login: "aidarbek", full: "aidarbek", mail: "a@mail.com", role:['admin', 'mentor'],editing: false}];// Binds Users list between Controllers
+	return [{login: "aidarbek", full: "aidarbek", mail: "a@mail.com", role:['admin', 'mentor'],editing: false}];// Binds Users list between Controllers
 });
 
 App.controller('User', ['$scope','Users' , '$http', function ($scope, Users, $http) {
-	
 	$scope.Users = Users;
-	$scope.userCreation = {role: []};
+	$scope.userCreation = {};
 	$scope.err = false;
 	$scope.isAdmin = true;
 	$scope.registring = false;
 	$scope.roles = ['student', 'mentor', 'admin'];
-	$scope.Role = function(role, index)
-	{
-		//console.log(role);
-		if(typeof index === 'undefined')
-		{
-			if($scope.userCreation.role.indexOf(role) > -1)
-			{
-				$scope.userCreation.role.splice(role, 1);
-			}
-			else
-			{
-				$scope.userCreation.role.push(role);
-			}
-			//console.log($scope.userCreation.role);
-		}
-		else
-		{	
-			if($scope.Users[index].role.indexOf(role) > -1)
-			{
-				$scope.Users[index].role.splice($scope.Users[index].role.indexOf(role), 1);
-			}
-			else
-			{
-				$scope.Users[index].role.push(role);
-			}
-		}
-	}
-	$scope.Checked = function(role, index)
-	{
-		if($scope.Users[index].role.indexOf(role) > -1)
-			return "Checked";
-		else
-			return "";
-	}
 	$scope.Add = function()
 	{
 		$scope.registring = true;
 	}
 	$scope.AddToArray = function()
 	{
-		AddUser($scope);
+		AddUser($scope, $scope.userCreation.role);
 		$scope.registring = false;
 	}
 	$scope.Delete = function(index)
 	{
-		var url = "";//We send a GET request on this URL, in which we say that 
-					//we want to delete a user with specific ID
+		var url = "";
+		//We send a GET request on this URL, in which we say that 
+		//we want to delete a user with specific ID
 		// Should be commented till BE is not available
 		/*
 			$http.get(url).success(function(data){	
@@ -125,14 +102,22 @@ App.controller('User', ['$scope','Users' , '$http', function ($scope, Users, $ht
 	}
 	$scope.Save = function(index)
 	{
-		var url = "";//We send a POST request here, which contain new information about 
-					// user with specific ID, which is described in $scopeUsers[index]
+		var url = "";
+		//We send a POST request here, which contain new information about 
+		// user with specific ID, which is described in $scopeUsers[index]
 		//Should be commented till BE is not available
 		/*
 			$http.post(url, $scope.Users[index]).success(function(data){
 				$scope.Users[index].editing = false;
 			});
 		*/
+		for(var i = 0; i < Users[index].role.length; i++)
+		{
+			if($scope.roles.indexOf($scope.Users[index].role[i]) == -1)
+			{
+				$scope.roles.push($scope.Users[index].role[i]);
+			}
+		}
 		$scope.Users[index].editing = false;
 	}
 }]);
